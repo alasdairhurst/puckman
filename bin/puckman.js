8e2f4ca803c848a501af4b2617eb80ff40a47678
Alasdair Hurst <omega@omegahurst.co.uk>
2017-07-23 22:14:41 +0100

    issue-1 can now run commands from inside modules

--- bin/puckman.js
+++ bin/puckman.js
@@ -10,33 +10,45 @@ const Loader = require('../lib/loader');
 const commands = require('../lib/commands');
 const utils = require('../lib/utils');
 
-Loader.loadConfig()
-	.then(config => {
-		config.getModules().forEach(mod => {
-			Object.keys(mod.scripts).forEach(hook => {
-				utils.registerHook(mod.name, hook, mod.scripts[hook]);
+const loadConfig = () => {
+	return Loader.loadConfig()
+		.then(config => {
+			config.getModules().forEach(mod => {
+				Object.keys(mod.scripts).forEach(hook => {
+					utils.registerHook(mod.name, hook, mod.scripts[hook]);
+				});
 			});
+			return Promise.resolve(config);
 		});
+};
+program
+	.version(packageJSON.version);
 
-		program
-			.version(packageJSON.version);
-
-		program
-			.command('clone [module]')
-			.alias('c')
-			.action((moduleName) => {
-				commands.clone(config, moduleName);
+program
+	.command('clone [module]')
+	.alias('c')
+	.action((moduleName) => {
+		loadConfig()
+			.then(config => {
+				return commands.clone(config, moduleName);
+			})
+			.catch(err => {
+				logger.error(err.message ? err.message : err);
 			});
-
-		program
-			.command('install [module]')
-			.alias('i')
-			.action((moduleName) => {
-				commands.install(config, moduleName);
-			});
-
-		program.parse(process.argv);
-	})
-	.catch(err => {
-		logger.error(err.message ? err.message : err);
 	});
+
+program
+	.command('install [module]')
+	.alias('i')
+	.action((moduleName) => {
+		loadConfig()
+			.then(config => {
+				return commands.install(config, moduleName);
+			})
+			.catch(err => {
+				logger.error(err.message ? err.message : err);
+			});
+	});
+
+program.parse(process.argv);
+
